7d65b96ace338bc616458a7f40046b84518164fb
suusara <smataeva09@gmail.com>
2023-05-10 13:54:26 +0600

    style contacts page

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -8,22 +8,22 @@ export const ContactsPage = () => {
         <p className="contacts_letter">You can sent me a letter right from here</p>
      <div className="contacts_block">
       <div className="contacts_email"> 
-        <input type="text" placeholder="name" />
-        <input type="email" placeholder="email" />
+        <input className="contacts_input" type="text" placeholder="name" />
+        <input className="contacts_input" type="email" placeholder="email" />
          <input className="contacts_file" type="file"  placeholder="file"/> 
-        <textarea name="" id="" cols="30" rows="10">Your message here...</textarea>
+        <textarea  name="" id="" cols="30" rows="10">Your message here...</textarea>
         <a href="mailto:smataeva09@gmail.com">
-        <button>Send message</button></a>
+        <button className="contacts_button">Send message</button></a>
      </div>
      <div className="contacts_a">
       <a href="https://www.linkedin.com/in/s-mataeva-5a0270261/">
-         <button>Go to LinkedIn</button></a>
+         <button className="contacts_button">Go to LinkedIn</button></a>
       <a href="tg://resolve?domain=M S">
-         <button>Chat with Telegram</button></a>
+         <button className="contacts_button">Chat with Telegram</button></a>
       <a href="https://github.com/sarausen">
-         <button>Go to GitHub</button></a>
+         <button className="contacts_button">Go to GitHub</button></a>
       <a href="https://www.behance.net/smataeva">
-         <button>Behance</button></a>
+         <button className="contacts_button">Behance</button></a>
      <div className="contacts_contact">
       <p>Kyrgyz Republic, Bishkek</p>
       <p>smataeva09@gmail.com</p>
