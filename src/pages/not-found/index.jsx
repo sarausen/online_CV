3ffcd9e5b7636f3bdb8edba10f1401adb6c59d96
suusara <smataeva09@gmail.com>
2023-05-16 21:30:07 +0600

    add found page

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,8 +1,9 @@
+import "./style.css"
 export const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
+    <div className="notfoundpage">
+      <h1 className="notfoundpage_h">404</h1>
+      <p className="notfoundpage_p">Page not found</p>
     </div>
   )
 }
