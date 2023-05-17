9ca7dcedec403e3122a4ca2ebba8d157b93fa3c8
suusara <smataeva09@gmail.com>
2023-05-17 17:36:10 +0600

    new text

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -7,7 +7,8 @@ export const AboutPage = () => {
         <h2>About me</h2>
         <p className="about_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
         <img className="about_img" src="https://images.pexels.com/photos/6347912/pexels-photo-6347912.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-        <p className="about_text">Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris.  Sed volutpat lacus vitae gravida viverra. Fusce vel tempor elit.  Aliquam sem ipsum, vehicula ac tortor vel, egestas ullamcorper dui. Curabitur at risus sodales, tristique est id, euismod justo. Mauris nec leo non libero sodales lobortis. Quisque a neque pretium, dictum tellus vitae, euismod neque. Nulla facilisi. Phasellus ultricies dignissim nibh ut cursus. Nam et quam sit amet turpis finibus maximus tempor eget augue. Aenean at ultricies lorem. Sed egestas ligula tortor, sit amet mattis ex feugiat non. Duis purus diam, dictum et ante at, commodo iaculis urna. Aenean lacinia, nisl id vehicula condimentum, enim massa.</p>
+        <p className="about_text">
+I enjoy working on interesting and challenging projects. I have experience both working in teams and independently. I have a strong desire and determination to develop my skills in frontend development.</p>
       </div>
     </div>
   )
